cf8d95036a3f3d9b1abb38f6fbafc09d92e4dfd9
stove <stove99@gmail.com>
2019-06-18 20:03:09 +0900

    매뉴얼 등록

---
+++ assets/data.js
@@ -0,0 +1,94 @@
+/** 이북 데이터 자료 정의
+**/
+var eBookData = {
+
+	password			: "",
+	useDebug			: false,
+	pageExt				: 'svg',
+	totalPageNum	: 15,
+	useLogo				: false,
+	logoUrl				: "http://",
+	homeUrl				: "http://",
+	useBooklist		: false,
+	
+	pageView : { /** 페이지 넘김 속성 */
+		type			: "flip",
+		side			: "auto",
+		duration	: 300,
+		cover			: true,
+	},
+	
+pageshadow:false,
+pagearea:true,
+
+MOKCHA:"true",
+BOOKMARK:"true",
+AUTOSKIP:"true",
+PRINT:"true",
+SUMNAIL:"true",
+TWITTER:"true",
+FACEBOOK:"true",
+SEARCH:"true",
+PDFDOWN:"true",
+MEMO:"true",
+SKETCH:"true",
+RANDOM:17035,
+
+
+	bookList : [ /** 이전호 목록 */
+	],
+	
+	tableList : [ /** 목차 */
+	],
+	
+	pageContents : [ /** 페이지 컨텐츠 */
+	],
+	
+	textList : [ /** 본문 검색 */
+		/*1*/
+		"",
+
+		/*2*/
+		"",
+
+		/*3*/
+		"",
+
+		/*4*/
+		"",
+
+		/*5*/
+		"",
+
+		/*6*/
+		"",
+
+		/*7*/
+		"",
+
+		/*8*/
+		"",
+
+		/*9*/
+		"",
+
+		/*10*/
+		"",
+
+		/*11*/
+		"",
+
+		/*12*/
+		"",
+
+		/*13*/
+		"",
+
+		/*14*/
+		"",
+
+		/*15*/
+		"",
+
+	],
+};
